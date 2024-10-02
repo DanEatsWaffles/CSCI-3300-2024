9a1a3e2d35af12743f4597c3a02c6a6f7108f619
Daniel S Winsness <143055484+DanEatsWaffles@users.noreply.github.com>
2024-10-02 17:00:47 -0400

    Add files via upload

---
+++ Apple/ZigZagApp/ZigZagAppTests/ZigZagAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZigZagAppTests.swift
+//  ZigZagAppTests
+//
+//  Created by Daniel W on 10/2/24.
+//
+
+import Testing
+@testable import ZigZagApp
+
+struct ZigZagAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
